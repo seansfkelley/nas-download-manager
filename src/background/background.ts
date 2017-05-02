0e85700da770ba3ad2efcf4c8b7230e747ee9360
Sean Kelley <seansfkelley@gmail.com>
2017-05-02 10:57:56 -0700

    Don't update the completed task list if the last fetch failed.

--- src/background/background.ts
+++ src/background/background.ts
@@ -17,7 +17,7 @@ onStoredStateChange(storedState => {
 
 onStoredStateChange(storedState => {
   // TODO: Also ignore if there's a failure message set.
-  if (storedState.tasksFetchUpdateTimestamp > START_TIME) {
+  if (storedState.tasksFetchUpdateTimestamp > START_TIME && storedState.tasksFetchFailureMessage == null) {
     const updatedFinishedTaskIds = storedState.tasks
       .filter(t => t.status === 'finished' || t.status === 'seeding')
       .map(t => t.id);
