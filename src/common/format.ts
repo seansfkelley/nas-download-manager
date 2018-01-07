a7f1e0247edbc3f617a927553fd62922f5503da6
Sean Kelley <seansfkelley@gmail.com>
2018-01-07 12:48:54 -0500

    Revert "No point formatting without the decimals, really..."
    This reverts commit a94553e6d85084542e38d7494e7e94c510e6afe0.
    
    Fixes #54.

--- src/common/format.ts
+++ src/common/format.ts
@@ -9,7 +9,11 @@ const METRIC_SUFFIXES = [
 
 export function formatMetric1024(n: number) {
   function renderString(suffix: string) {
-    return `${n.toFixed(2)}${suffix}`;
+    if (n === Math.round(n)) {
+      return `${n.toFixed(0)}${suffix}`;
+    } else {
+      return `${n.toFixed(2)}${suffix}`;
+    }
   }
 
   for (let i = 0; i < METRIC_SUFFIXES.length - 1; ++i) {
