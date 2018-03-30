83dbbe986da66a805266132cc96c655fbb577fac
Sean Kelley <seansfkelley@gmail.com>
2018-03-29 22:03:17 -0700

    Update typescript. Include test for erroneous version 2.

--- test/test-state.ts
+++ test/test-state.ts
@@ -6,6 +6,9 @@ import { _updateStateToLatest, State } from '../src/common/state';
 import { State_1 } from '../src/common/state/1';
 import { State_2 } from '../src/common/state/2';
 
+type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
+type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+
 interface PreVersioningState_0 {
   connection: {
     protocol: 'http' | 'https';
@@ -202,6 +205,37 @@ describe('state versioning', () => {
       tasksLastCompletedFetchTimestamp: 0,
       tasksLastInitiatedFetchTimestamp: 0,
       shouldHandleDownloadLinks: true,
+      stateVersion: 2,
+    };
+
+    expect(_updateStateToLatest(before)).to.deep.equal(UP_TO_DATE_STATE);
+  });
+
+  it('should update to the latest version from an erroneous version 2 missing fields', () => {
+    const before: Omit<State_2, 'taskSortType' | 'shouldHandleDownloadLinks'> = {
+      connection: {
+        protocol: 'http',
+        hostname: 'hostname',
+        port: 0,
+        username: 'username',
+        password: 'password',
+      },
+      visibleTasks: {
+        downloading: true,
+        uploading: true,
+        completed: true,
+        errored: true,
+        other: true,
+      },
+      notifications: {
+        enableCompletionNotifications: true,
+        enableFeedbackNotifications: true,
+        completionPollingInterval: 0,
+      },
+      tasks: [ DUMMY_TASK ],
+      taskFetchFailureReason: 'missing-config',
+      tasksLastCompletedFetchTimestamp: 0,
+      tasksLastInitiatedFetchTimestamp: 0,
       stateVersion: 2,
     };
 
