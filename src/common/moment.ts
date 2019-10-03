e424b451d75314de6c6d3781e5573b634eaf0467
Sean Kelley <seansfkelley@gmail.com>
2019-10-03 07:56:11 -0700

    Fix moment.js localization to fall back onto en. Fixes #124.

--- src/common/moment.ts
+++ src/common/moment.ts
@@ -1,6 +1,11 @@
 import moment from "moment";
 import "moment/locale/ru";
 
-moment.locale(browser.i18n.getUILanguage());
+// Explicitly fall back onto `en` as the default. For "backwards compatibility" (for the last
+// five years!), defining a locale will cause moment to silently set that locale as the
+// global default. This means that for users who didn't have their browser set to a language
+// that we have available here, they would get localizations in the most-recently-imported
+// locale rather than `en`, which is the default.
+moment.locale([browser.i18n.getUILanguage(), "en"]);
 
 export { moment };
