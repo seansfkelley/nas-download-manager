04f5dfe1b8d9917722d7072225a5a5f34aeb6ddd
Sean Kelley <seansfkelley@gmail.com>
2020-03-14 12:40:47 -0700

    Upgrade language version and enable (and fix) more strict errors.

--- src/common/lang.ts
+++ src/common/lang.ts
@@ -2,6 +2,10 @@ export function assertNever(n: never): never {
   throw new Error(`never assertion failed, got value ${n}`);
 }
 
+export function recordKeys<T extends string>(o: Record<T, any>): T[] {
+  return Object.keys(o) as T[];
+}
+
 export function typesafeUnionMembers<T extends string>(keys: Record<T, any>): T[] {
   return Object.keys(keys) as T[];
 }
