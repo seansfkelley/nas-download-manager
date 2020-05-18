7699bbbb11ac00784983aeee243b436683a52844
Sean Kelley <seansfkelley@gmail.com>
2020-05-17 18:44:03 -0700

    Add messages for task-management actions.

--- src/common/apis/messages.ts
+++ src/common/apis/messages.ts
@@ -1,40 +1,90 @@
 export enum MessageType {
   ADD_TASKS,
   POLL_TASKS,
+  PAUSE_TASK,
+  RESUME_TASK,
+  DELETE_TASK,
 }
 
-export interface AddTasksMessage {
-  type: typeof MessageType.ADD_TASKS;
+interface Message<T extends MessageType> {
+  type: T;
+}
+
+function makeTypeGuard<T extends Message<MessageType>>(type: T["type"]) {
+  return (m: object | null | undefined): m is T => {
+    return m != null && (m as any).type == type;
+  };
+}
+
+function sendMessage<T extends Message<MessageType>>(m: T) {
+  browser.runtime.sendMessage(m);
+}
+
+export interface AddTasksMessage extends Message<MessageType.ADD_TASKS> {
   urls: string[];
   path?: string;
 }
 
 export const AddTasksMessage = {
   send: (urls: string[], path?: string) => {
-    const message: AddTasksMessage = {
+    sendMessage<AddTasksMessage>({
       type: MessageType.ADD_TASKS,
       urls,
       path,
-    };
-    browser.runtime.sendMessage(message);
+    });
   },
-  is: (message: object | null | undefined): message is AddTasksMessage => {
-    return message != null && (message as any).type === MessageType.ADD_TASKS;
-  },
+  is: makeTypeGuard<AddTasksMessage>(MessageType.ADD_TASKS),
 };
 
-export interface PollTasksMessage {
-  type: typeof MessageType.POLL_TASKS;
-}
+export interface PollTasksMessage extends Message<MessageType.POLL_TASKS> {}
 
 export const PollTasksMessage = {
   send: () => {
-    const message: PollTasksMessage = {
+    sendMessage<PollTasksMessage>({
       type: MessageType.POLL_TASKS,
-    };
-    browser.runtime.sendMessage(message);
+    });
   },
-  is: (message: object | null | undefined): message is PollTasksMessage => {
-    return message != null && (message as any).type === MessageType.POLL_TASKS;
+  is: makeTypeGuard<PollTasksMessage>(MessageType.POLL_TASKS),
+};
+
+export interface PauseTaskMessage extends Message<MessageType.PAUSE_TASK> {
+  taskId: string;
+}
+
+export const PauseTaskMessage = {
+  send: (taskId: string) => {
+    sendMessage<PauseTaskMessage>({
+      type: MessageType.PAUSE_TASK,
+      taskId,
+    });
   },
+  is: makeTypeGuard<PauseTaskMessage>(MessageType.PAUSE_TASK),
+};
+
+export interface ResumeTaskMessage extends Message<MessageType.RESUME_TASK> {
+  taskId: string;
+}
+
+export const ResumeTaskMessage = {
+  send: (taskId: string) => {
+    sendMessage<ResumeTaskMessage>({
+      type: MessageType.RESUME_TASK,
+      taskId,
+    });
+  },
+  is: makeTypeGuard<ResumeTaskMessage>(MessageType.RESUME_TASK),
+};
+
+export interface DeleteTaskMessage extends Message<MessageType.DELETE_TASK> {
+  taskId: string;
+}
+
+export const DeleteTaskMessage = {
+  send: (taskId: string) => {
+    sendMessage<DeleteTaskMessage>({
+      type: MessageType.DELETE_TASK,
+      taskId,
+    });
+  },
+  is: makeTypeGuard<DeleteTaskMessage>(MessageType.DELETE_TASK),
 };
