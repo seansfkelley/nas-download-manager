d10a2707192236777ce2f4de324eac8c5d144c7d
Sean Kelley <seansfkelley@users.noreply.github.com>
2026-08-11 12:42:03 -0700

    Adjust lint rules to match removed compiler options.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -67,7 +67,14 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-unused-vars": [
         "error",
-        { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
       ],
       "@typescript-eslint/no-explicit-any": "off", // TODO
       "@typescript-eslint/no-empty-object-type": "off", // TODO
